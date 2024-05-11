0a38520aab5c89e309eb4098ce01b4dd05580d02
cadub <cadubas1@gmail.com>
2024-05-11 12:34:14 -0300

    feat: adding new types, changing types that already existed and altering the data sended by the API

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { Hidrometer, SmartLight, WaterTank } from "./core/models/interface";
+import { Hidrometer, HidrometerNodeField, SmartLight, SmartLightNodeField, WaterTank, WaterTankNodeField } from "./core/models/interface";
 import express from 'express';
 import dotenv from 'dotenv';
 import { InfluxDB, flux } from '@influxdata/influxdb-client';
@@ -19,65 +19,91 @@ const org : string = process.env.ORGANIZATION!
 const client = new InfluxDB({url: url, token: token})
 
 app.get("/api/smartlights", async (req : Request, res : Response) =>  {
-    const smartLightsByNode : {[key: string] : {[key: string] : (number | string)[]} }= {}
+    const smartLightsByNode : SmartLight = {}
     const queryApi = client.getQueryApi(org);
 
     const query = flux`from(bucket: "${bucket}")
     |> range(start: -1h) 
     |> filter(fn: (r) => r._measurement == "SmartLight")
     |> limit(n: 10)`
-    const result : SmartLight[] = await queryApi.collectRows(query);
-    result.map((value) => {
-        if(!smartLightsByNode[value.nodeName]) {
-            smartLightsByNode[value.nodeName] = {}
+    const result : SmartLightNodeField[] = await queryApi.collectRows(query);
+    result.map((smartLightNodeField) => {
+        const smartlightsValue = {
+            fieldValue: smartLightNodeField._value,
+            time: smartLightNodeField._time,
+            start: smartLightNodeField._start,
+            stop: smartLightNodeField._stop
         }
-        if(!smartLightsByNode[value.nodeName][value._field]) {
-            smartLightsByNode[value.nodeName][value._field] = [value._value]
+        if(!smartLightsByNode[smartLightNodeField.nodeName]) {
+            smartLightsByNode[smartLightNodeField.nodeName] = {}
         }
-        smartLightsByNode[value.nodeName][value._field].push(value._value)
+        if(!smartLightsByNode[smartLightNodeField.nodeName][smartLightNodeField._field]) {
+            smartLightsByNode[smartLightNodeField.nodeName][smartLightNodeField._field] = [smartlightsValue]
+        } else {
+            smartLightsByNode[smartLightNodeField.nodeName][smartLightNodeField._field].push(smartlightsValue)
+        }
     })
+    res.status(200)
     res.send(smartLightsByNode)
 })
 
 app.get("/api/watertanklevel", async (req : Request, res : Response) =>  {
-    const waterTankLevelByNode : {[key: string] : {[key: string] : (number | string)[]} } = {}
+    const waterTankLevelByNode : WaterTank = {}
     const queryApi = client.getQueryApi(org);
 
     const query = flux`from(bucket: "${bucket}")
-    |> range(start: -1h) 
+    |> range(start: -20m) 
     |> filter(fn: (r) => r._measurement == "WaterTankLavel")
     |> limit(n: 10)`
-    const result : WaterTank[] = await queryApi.collectRows(query);
-    result.map((value) => {
-        if(!waterTankLevelByNode[value.nodeName]) {
-            waterTankLevelByNode[value.nodeName] = {}
+    const result : WaterTankNodeField[] = await queryApi.collectRows(query);
+    result.map((waterTankNodeField) => {
+        const waterTankValue = {
+            fieldValue: waterTankNodeField._value,
+            time: waterTankNodeField._time,
+            start: waterTankNodeField._start,
+            stop: waterTankNodeField._stop
         }
-        if(!waterTankLevelByNode[value.nodeName][value._field]) {
-            waterTankLevelByNode[value.nodeName][value._field] = [value._value]
+        if(!waterTankLevelByNode[waterTankNodeField.nodeName]) {
+            waterTankLevelByNode[waterTankNodeField.nodeName] = {}
         }
-        waterTankLevelByNode[value.nodeName][value._field].push(value._value)
+        if(!waterTankLevelByNode[waterTankNodeField.nodeName][waterTankNodeField._field]) {
+           
+            waterTankLevelByNode[waterTankNodeField.nodeName][waterTankNodeField._field] = [waterTankValue]
+        }else {
+            waterTankLevelByNode[waterTankNodeField.nodeName][waterTankNodeField._field].push(waterTankValue)
+        }
     })
+    res.status(200)
     res.send(waterTankLevelByNode)
 })
 
 app.get("/api/hidrometer", async (req : Request, res : Response) =>  {
-    const hidrometerByNode : {[key: string] : {[key: string] : (number | string)[]} } = {}
+    const hidrometerByNode : Hidrometer = {}
     const queryApi = client.getQueryApi(org);
 
     const query = flux`from(bucket: "${bucket}")
-    |> range(start: -1h) 
-    |> filter(fn: (r) => r._measurement == "Hidrometer")`
-    const result : Hidrometer[] =  await queryApi.collectRows(query);
-    result.map((value) => {
-        if(!hidrometerByNode[value.nodeName]) {
-            hidrometerByNode[value.nodeName] = {}
+    |> range(start: -20m) 
+    |> filter(fn: (r) => r._measurement == "Hidrometer")
+    |> limit(n: 10)`
+    const result : HidrometerNodeField[] =  await queryApi.collectRows(query);
+    result.map((hidrometerNodeField) => {
+        const hidrometerValue = {
+            fieldValue: hidrometerNodeField._value,
+            time: hidrometerNodeField._time,
+            start: hidrometerNodeField._start,
+            stop: hidrometerNodeField._stop
         }
-        if(!hidrometerByNode[value.nodeName][value._field]) {
-            hidrometerByNode[value.nodeName][value._field] = [value._value]
+        if(!hidrometerByNode[hidrometerNodeField.nodeName]) {
+            hidrometerByNode[hidrometerNodeField.nodeName] = {}
         }
-        hidrometerByNode[value.nodeName][value._field].push(value._value)
+        if(!hidrometerByNode[hidrometerNodeField.nodeName][hidrometerNodeField._field]) {
+            hidrometerByNode[hidrometerNodeField.nodeName][hidrometerNodeField._field] = [hidrometerValue]
+        }
+        else {
+            hidrometerByNode[hidrometerNodeField.nodeName][hidrometerNodeField._field].push(hidrometerValue)
+        }
     })
-
+    res.status(200)
     res.send(hidrometerByNode)
 })
 
